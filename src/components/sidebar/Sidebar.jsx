2b06bbad0b56a1ec34a16051af60d477673e71d2
FarshidMousavi <r.shidef@gmail.com>
2022-04-04 16:06:19 +0430

    create sidebar component and add user section

---
+++ src/components/sidebar/Sidebar.jsx
@@ -0,0 +1,89 @@
+import React, { useContext, useEffect, useState } from "react";
+import { Link, NavLink } from "react-router-dom";
+import { AiFillHome } from "react-icons/ai";
+import { BsPeople } from "react-icons/bs";
+import { RiLiveLine } from "react-icons/ri";
+import badge from "../../assets/images/tiktok_ver.png";
+import "./sidebar.css";
+import { userContext } from "../../context/UserContex";
+
+const Sidebar = () => {
+  const users = useContext(userContext);
+  const allUser = 20;
+  return (
+    <div className="tiktok__sidebar">
+      <div className="tiktok__sidebar-mainContainer">
+        <div>
+          <NavLink
+            to="/"
+            className={(navData) =>
+              navData.isActive ? "navLink active" : "navLink"
+            }
+          >
+            <AiFillHome />
+            <h4>For You</h4>
+          </NavLink>
+        </div>
+        <div>
+          <NavLink
+            to="/following"
+            className={(navData) =>
+              navData.isActive ? "navLink active" : "navLink"
+            }
+            onClick={(prevState) => console.log(prevState)}
+          >
+            <BsPeople />
+
+            <h4>Following</h4>
+          </NavLink>
+        </div>
+        <div>
+          <NavLink
+            to="/live"
+            className={(navData) =>
+              navData.isActive ? "navLink active" : "navLink"
+            }
+          >
+            <RiLiveLine />
+            <h4>Live</h4>
+          </NavLink>
+        </div>
+        <div></div>
+        <div></div>
+      </div>
+      <div className="tiktok__sidebar-frameContainer">
+        <p>Log in to follow creators, like videos, and view comments.</p>
+        <button>Log in</button>
+      </div>
+      <div className="tiktok__sidebar-userContainer">
+        <p>Suggested accounts</p>
+        {users.slice(0, allUser).map(({ user }) => {
+          return (
+            <div className="userContainer" key={user.uid}>
+              <Link to="/" className="suggest-user-avatar">
+                <div>
+                  <span>
+                    <img src={user.avatar_thumb.url_list[0]} alt="" />
+                  </span>
+                </div>
+              </Link>
+              <Link to="/" className="suggest-user-content">
+                <div className="userTitleWrapper">
+                  <h4 className="user-title">{user.unique_id}</h4>
+                  <div className="user-bluev">
+                    {user.custom_verify === "Verified account" && (
+                      <img src={badge} alt="" />
+                    )}
+                  </div>
+                </div>
+                <p className="userDesc">{user.nickname}</p>
+              </Link>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,11 +1,18 @@
 import React from "react";
 import Navbar from "../components/navbar/Navbar";
+import Sidebar from "../components/sidebar/Sidebar";
+import "./layout.css";
 
 const Layout = ({ children }) => {
   return (
     <>
       <Navbar />
-      {children}
+      <div className="bodyContainer">
+        <div className="sideBarContainer">
+          <Sidebar />
+        </div>
+        <div className="mainContainer">{children}</div>
+      </div>
     </>
   );
 };
