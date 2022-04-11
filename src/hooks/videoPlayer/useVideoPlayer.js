36e9fbbdc5fe90f06f6d9cd52382dad5179b7652
FarshidMousavi <r.shidef@gmail.com>
2022-04-11 12:40:54 +0430

    add volume controller and fix progress bar value on load

--- src/hooks/videoPlayer/useVideoPlayer.js
+++ src/hooks/videoPlayer/useVideoPlayer.js
@@ -5,7 +5,7 @@ const useVideoPlayer = (videoElement) => {
   const [progress, setProgress] = useState(0);
   const [speed, setSpeed] = useState(1);
   const [isMuted, setIsMuted] = useState(false);
-
+  const [volume, setVolume] = useState(1);
   const togglePlay = () => {
     setIsPlaying(!isPlaying);
   };
@@ -15,9 +15,11 @@ const useVideoPlayer = (videoElement) => {
 
   const handleOnTimeUpdate = () => {
     const progressUpdate =
-      (videoElement.current.currentTime / videoElement.current.duration) * 100;
+      (videoElement.current.currentTime / videoElement.current.duration) *
+        100 || 0; // on load compunent value was load NAN but now load 0
     setProgress(progressUpdate);
   };
+
   const handleVideoProgress = (event) => {
     const manualChange = Number(event.target.value);
     videoElement.current.currentTime =
@@ -40,16 +42,24 @@ const useVideoPlayer = (videoElement) => {
       ? (videoElement.current.muted = true)
       : (videoElement.current.muted = false);
   }, [isMuted, videoElement]);
+
+  const handleVolume = (event) => {
+    const videoVolume = Number(event.target.value);
+    videoElement.current.volume = videoVolume;
+    setVolume(videoVolume);
+  };
   return {
     isPlaying,
     progress,
     speed,
     isMuted,
+    volume,
     togglePlay,
     handleOnTimeUpdate,
     handleVideoProgress,
     handleVideoSpeed,
     toggleMute,
+    handleVolume,
   };
 };
 
