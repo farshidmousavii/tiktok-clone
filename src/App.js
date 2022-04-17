920974903f65fd5d17dee4da79e60c9734c71627
FarshidMousavi <r.shidef@gmail.com>
2022-04-17 11:51:35 +0430

    change layout inside routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { useState, useEffect } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
-import Layout from "./layout/Layout";
+import Layout from "./layouts/Layout";
 import { userContext } from "./context/UserContex";
 import { getSuggestedUsers } from "./services/APIServices";
 import Home from "./pages/home/Home";
+import Profile from "./pages/profile/Profile";
+import SecondaryLayout from "./layouts/SecondaryLayout";
 const App = () => {
   const [users, setUsers] = useState(null);
 
@@ -22,13 +24,43 @@ const App = () => {
   return (
     <BrowserRouter>
       <userContext.Provider value={users}>
-        <Layout>
-          <Routes>
-            <Route path="/">
-              <Route index element={<Home />} />
-            </Route>
-          </Routes>
-        </Layout>
+        <Routes>
+          <Route path="/">
+            <Route
+              index
+              element={
+                <Layout>
+                  <Home />
+                </Layout>
+              }
+            />
+            <Route
+              path=":username"
+              element={
+                <SecondaryLayout>
+                  <Profile />
+                </SecondaryLayout>
+              }
+            />
+          </Route>
+          <Route
+            path="/following"
+            element={
+              <Layout>
+                <> following </>
+              </Layout>
+            }
+          />
+          <Route
+            path="/live"
+            element={
+              <SecondaryLayout>
+                <> Live </>
+              </SecondaryLayout>
+            }
+          />
+          <Route path="*" element={<p> Noth found</p>} />
+        </Routes>
       </userContext.Provider>
     </BrowserRouter>
   );
