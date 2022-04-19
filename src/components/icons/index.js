7ce5aae243ceb087d5699157e32e3287c4b98f41
FarshidMousavi <r.shidef@gmail.com>
2022-04-19 12:56:38 +0430

    Create icon component and create all of icons by svg

---
+++ src/components/icons/index.js
@@ -0,0 +1,11 @@
+export { default as Email } from "./Email";
+export { default as Embeded } from "./Embeded";
+export { default as Facebook } from "./Facebook";
+export { default as Friends } from "./Friends";
+export { default as Line } from "./Line";
+export { default as Linkdin } from "./Linkdin";
+export { default as Pinterest } from "./Pinterest";
+export { default as Reddit } from "./Reddit";
+export { default as Telegram } from "./Telegram";
+export { default as Twitter } from "./Twitter";
+export { default as Whatsapp } from "./Whatsapp";
