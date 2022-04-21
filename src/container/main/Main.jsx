94e5d4c9510120d5c1978c6696f9e08522a32d33
FarshidMousavi <r.shidef@gmail.com>
2022-04-21 10:06:09 +0430

    add share component to main page

--- src/container/main/Main.jsx
+++ src/container/main/Main.jsx
@@ -9,10 +9,19 @@ import Video from "../../components/video/Video";
 import badge from "../../assets/images/tiktok_ver.png";
 import "./main.css";
 import NumberFormatter from "../../utils/numberFormatter";
+import Share from "../../components/share/Share";
 
 const Main = () => {
   const [feeds, setFeeds] = useState(null);
+  const [share, setShare] = useState(false);
+  const [show, setShow] = useState(false);
 
+  const handleClick = (e, id) => {
+    if (e.target.id === id) {
+      setShare(true);
+      setShow(true);
+    }
+  };
   const getAllFeeds = async () => {
     try {
       const { data } = await getFeed();
@@ -95,7 +104,7 @@ const Main = () => {
               </div>
               <div className="feed_video-actions">
                 <button className="video__sideBar-actionItem">
-                  <span>
+                  <span className="video_sideBar-actionItem_span">
                     <BsHeartFill />
                   </span>
                   <strong className="like-count">
@@ -103,21 +112,35 @@ const Main = () => {
                   </strong>
                 </button>
                 <button className="video__sideBar-actionItem">
-                  <span>
+                  <span className="video_sideBar-actionItem_span">
                     <FaCommentDots />
                   </span>
                   <strong className="comment-count">
                     {NumberFormatter(feed.statistics.comment_count)}
                   </strong>
                 </button>
-                <button className="video__sideBar-actionItem">
-                  <span>
-                    <RiShareForwardFill style={{ fontSize: "25px" }} />
+                <button
+                  className="video__sideBar-actionItem"
+                  // onMouseOver={() => setShow(true)}
+                  id={feed.aweme_id}
+                  onClick={(e) => handleClick(e, feed.aweme_id)}
+                >
+                  <span
+                    className="video_sideBar-actionItem_span"
+                    id={feed.aweme_id}
+                  >
+                    <RiShareForwardFill
+                      style={{ fontSize: "25px" }}
+                      id={feed.aweme_id}
+                    />
                   </span>
                   <strong className="share-count">
                     {NumberFormatter(feed.statistics.share_count)}
                   </strong>
                 </button>
+                {share && (
+                  <Share show={show} handleShow={() => setShow(false)} />
+                )}
               </div>
             </div>
           </div>
